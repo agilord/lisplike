29412b890393b118d142ebf8a3d5adb467d8f1c0
Alexy Marcell <alexy.marcell@hotmail.com>
2021-12-05 21:05:49 +0100

    call_stack class structure is done. TODO: slib.dart handling.

--- lib/jlp.dart
+++ lib/jlp.dart
@@ -3,28 +3,32 @@ import 'slib.dart';
 abstract class Scope {
   String get name;
   Scope? parent;
-  Scope();
-  Scope._(this.parent);
+  Scope(this.parent);
   hasVar(String name) => false;
   getVar(String name) => ArgumentError("get $name at wrong place");
   setVar(String name, value) => parent!.setVar(name, value);
 
   resolv(String name) {
-    if(this.hasVar(name)) return getVar(name);
-    else return parent!.resolv(name);
+    if (hasVar(name)) {
+      return getVar(name);
+    } else {
+      return parent!.resolv(name);
+    }
   }
+
   moveUpValue(String name, value, [int level = 1]) {
-    if(this.hasVar(name)) {
-      this.setVar(name, value);
+    if (hasVar(name)) {
+      setVar(name, value);
       level -= 1;
     }
-    if(level > 0) {
+    if (level > 0) {
       parent!.moveUpValue(name, value);
     }
   }
+
   moveUp(String name, [int level = 1]) {
-    if(this.hasVar(name)) {
-      final value = this.getVar(name);
+    if (hasVar(name)) {
+      final value = getVar(name);
       moveUpValue(name, value, level);
     }
   }
@@ -34,6 +38,7 @@ abstract class Scope {
 class NativeScope extends Scope {
   @override
   String get name => 'nativescpe';
+  NativeScope(Scope? parent) : super(parent);
 
   // TODO: native functions
 }
@@ -43,24 +48,30 @@ class VarScope extends Scope {
   @override
   String get name => 'varscope';
   Map<String, dynamic> vars;
-  VarScope._(this.vars, Scope? parent) : super._(parent);
-  factory VarScope({Map<String, dynamic>? vars, Scope? parent}) {
+  VarScope._(this.vars, Scope? parent) : super(parent);
+  factory VarScope(Scope? parent, [Map<String, dynamic>? vars]) {
     vars ??= {if (parent != null) "#up": parent};
     return VarScope._(vars, parent);
   }
 
-  @override hasVar(String varname) {
+  @override
+  hasVar(String varname) {
     return vars.containsKey(varname);
   }
-  @override getVar(String name) {
+
+  @override
+  getVar(String name) {
     return vars[name];
   }
-  @override setVar(String name, value) {
+
+  @override
+  setVar(String name, value) {
     return vars[name] = value;
   }
 }
 
 abstract class Evaler extends Scope {
+  Evaler(Scope? parent) : super(parent);
   eval();
 }
 
@@ -69,43 +80,112 @@ class ValueEval extends Evaler {
   @override
   String get name => 'valueeval';
   dynamic value;
-  ValueEval(this.value);
-  eval() => eval_from_Evaluator(value);
+  ValueEval(this.value, Scope? parent) : super(parent);
+
+  @override
+  eval() {
+    if (value is List) {
+      var name = value.first;
+      var pars = List.from(value.sublist(1));
+
+      // name resolution
+      if (name is String) {
+        name = StringEval(name, this);
+      }
+
+      return FuncEval(name, pars, this);
+    }
+
+    if (value is String) {
+      return StringEval(value, this);
+    }
+
+    if (value is num) {
+      // can be int or double
+      return value.toDouble();
+    }
+
+    return value;
+  }
 }
 
 class FuncEval extends Evaler {
   @override
   String get name => 'funceval';
-  FuncEval(this.func, this.pars);
+  FuncEval(this.func, this.pars, Scope? parent) : super(parent);
   dynamic func;
   dynamic pars;
-  // TODO extract _funceval
+  @override
   eval() {
+    while (func is Evaler) {
+      func = func.eval();
+    }
 
+    if (func is List && func[0] == 'macrocall') {
+      func = ValueEval(func[1], this);
+      while (func is Evaler) {
+        func = func.eval();
+      }
+    } else {
+      pars = BlockEval(pars, this);
+    }
+    while (pars is Evaler) {
+      pars = pars.eval();
+    }
+
+    if (func is Function) {
+      // native call
+      return func(pars, this);
+    } else if (func[0] == 'lambda') {
+      // lambda call
+      final vars = VarScope(this);
+      for (var i = 0; i < pars.lenght && i < func[1].length; i++) {
+        vars.setVar(func[1][i], pars[i]);
+      }
+      return ValueEval(func[2], vars);
+    }
   }
 }
 
 class StringEval extends Evaler {
-  @override String get name => 'stringeval';
+  @override
+  String get name => 'stringeval';
   String str;
-  StringEval(this.str)
+  StringEval(this.str, Scope? parent) : super(parent);
   // TODO extract parseStr
-  @override eval() {
-
+  @override
+  eval() {
+    final first = str[0];
+    final rest = str.substring(1);
+    switch (first) {
+      case '\'':
+        return rest.endsWith('\'') ? rest.substring(0, rest.length - 1) : rest;
+      case '&':
+        {
+          final vr = resolv(rest);
+          return vr;
+        }
+      case ':':
+        return ["macrocall", rest];
+    }
+    return str;
   }
 }
 
 class BlockEval extends Evaler {
   @override
   String get name => 'blockeval';
-  List<dynamic> elems;
-  BlockEval(this.elems);
-  factory BlockEval.evalAll(List<dynamic> vals) {
-    return BlockEval(vals.map((e) => ValueEval(e)).toList()); 
+  List<dynamic> elems = [];
+  BlockEval.empty(Scope? parent) : super(parent);
+  factory BlockEval(List<dynamic> vals, Scope? parent) {
+    final block = BlockEval.empty(parent);
+    block.elems = vals.map((e) => ValueEval(e, block)).toList();
+    return block;
   }
+  @override
   eval() {
-    for(var i = 0; i < elems.length; ++i) {
-      while(elems[i] is Evaler) {
+    for (var i = 0; i < elems.length; ++i) {
+      while (elems[i] is Evaler) {
         elems[i] = elems[i].eval();
       }
     }
@@ -115,93 +195,16 @@ class BlockEval extends Evaler {
 
 class Evaluator {
   VarScope root;
-  VarScope scope;
-  Evaluator._(this.root, this.scope);
+  Evaluator._(this.root);
   factory Evaluator([VarScope? root]) {
-    root ??= VarScope();
-    return Evaluator._(root, root);
+    root ??= VarScope(null);
+    return Evaluator._(root);
   }
-
-  _funceval(name, List pars) {
-    if (name is String) {
-      return bfuns[name]!(pars, this);
-    }
-    if (name is List) {
-      scope = VarScope(parent: scope);
-      final parnames = name[1];
-      final body = name[2];
-      for (var i = 0; i < parnames.length; ++i) {
-        scope.setvar(parnames[i], pars[i]);
-      }
-      final ret = eval(body);
-      scope = scope.parent!;
-      return ret;
-    }
-  }
-
   // evaluates "elem" in 'eval' context.
   eval(elem) {
-    if (elem is List) {
-      var name = elem.first;
-      var pars = List.from(elem.sublist(1));
-      // name resolution
-      while (name is String) {
-        final val = parseStr(name);
-        if (val == name) {
-          break; // fix point
-        } else {
-          name = val;
-        }
-      }
-
-      // Base function call
-      if (name is String) {
-        for (int i = 0; i < pars.length; ++i) {
-          pars[i] = eval(pars[i]);
-        } // eval pars
-        return _funceval(name, pars);
-      }
-      // lambda function call
-      if (name is List && name[0] == 'lambda') {
-        for (int i = 0; i < pars.length; ++i) {
-          pars[i] = eval(pars[i]);
-        } // eval pars
-        return _funceval(name, pars);
-      }
-      // macrocall
-      if (name is List && name[0] == 'macrocall') {
-        return _funceval(name[1], pars); // no param eval
-      }
+    var val = ValueEval(elem, root);
+    while (val is Evaler) {
+      val = val.eval();
     }
-    if (elem is String) {
-      return parseStr(elem);
-    }
-    if (elem is num) {
-      //print(elem.runtimeType); // can be int or double if converted from JSON
-      return elem.toDouble();
-    }
-    if (elem is Map) {
-      return elem;
-    }
-    if (elem is bool || elem == null) {
-      return elem;
-    }
-  }
-
-  parseStr(String str) {
-    final first = str[0];
-    final rest = str.substring(1);
-    switch (first) {
-      case '\'':
-        return rest;
-      case '&':
-        {
-          final vr = eval([":resolve", rest]);
-          return vr;
-        }
-      case ':':
-        return ["macrocall", rest];
-    }
-    return str;
   }
 }
